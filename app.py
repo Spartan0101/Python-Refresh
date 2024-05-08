af1186336ebcbb8a40b6a43234bb5ae195fdddbc
Spartan1010 <tranulovianos@gmail.com>
2024-05-08 16:02:57 +0100

    Done

---
+++ app.py
@@ -0,0 +1,108 @@
+#variables
+#age = 20
+#price = 10.95
+#product_name = "Mosh"
+#is_discounted = True
+#print(age)
+#print("Hello World")
+
+#reciving input
+#name = input("What is your name? ")
+#print("Hello " + name)
+
+#birth_year = input("Enter your birth year: ")
+#age = 2024 - int(birth_year)
+#print(age)
+
+#firstVal = input ("First : ")  # or first = float(input("First: "))
+#secondVal = input ("Second : ")
+#sum = float(firstVal) + float(secondVal)  # or sum = first + second
+#print("Sum is : " + str(sum))
+
+#course = 'Python for beginners'
+#print(course.upper())
+#print(course.find('y'))
+#print('Python' in course)
+
+#Arithmatic Operators
+
+#print(10 + 3)
+#print(10/3)
+#print(10//3) # no decimals
+
+#x = 10
+#x *= 3
+#print(x)
+#Comparison operators
+
+#x = 3 > 2 # boolian expression
+#print(x)
+
+#logical operators
+
+#price = 25
+#print(price > 10 and price < 30 )   # inst of and can use OR / NOT
+
+#IF statements
+
+#temperature = 5
+#if temperature > 30:
+#    print("It's a hot day")
+#elif temperature > 20:
+#    print("Get a jacket!")
+#elif temperature > 10:
+#    print("It's cold!")
+#else:
+#    print("Don't go out :) !")
+#print("Done!")
+
+#execise
+
+#weight = float(input("Weight: "))
+#unit = input("(K)g or (L)bs: ")
+#if unit.upper() == "K":
+    #converted = weight / 0.45
+    #else:
+    #converted = weight * 0.45
+    #print("Weight in Kg: "+ str(converted))
+
+#Loops
+
+#i = 1
+#while i <= 5:
+   # print(i)
+   # i = i + 1
+
+#while i <= 10:
+    #print(i * "*")
+   # i = i + 1
+
+#numbers = [1,2,3,4,5]
+#for item in numbers:
+#    print(item)
+
+#Lists
+
+#names = ["Ianos","Tibi","Danka", "Sam"]
+#names[0] = "Gabi"
+#print(names[0])
+#print(names[-1])
+#print(names)
+#print(names[1:3])
+
+#numbers = [1,2,3,4,5]
+#numbers.append(6)
+#print(numbers)
+#print(1 in numbers)
+#print(len(numbers))
+
+#numbers = range(5)
+#print(numbers)
+#numbers = range(5, 10, 2)
+#for number in numbers: OR
+#for number in range(5):
+ #   print(number)
+
+ # Tuples immutable
+#numbers = (1, 2, 3)
+#print(numbers.count(3))
